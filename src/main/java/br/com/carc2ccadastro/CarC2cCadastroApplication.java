e2705a475d1e1120e7998f3d20988923b2ea4ace
gustavoaquino <guuaquino@hotmail.com>
2020-03-08 13:09:18 -0300

    initial commit

---
+++ src/main/java/br/com/carc2ccadastro/CarC2cCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.carc2ccadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarC2cCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarC2cCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carc2ccadastro/CarC2cCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carc2ccadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarC2cCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
