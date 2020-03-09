8fbba4024bc9c1f4752a9c73566417f39c7ea70b
gustavoaquino <guuaquino@hotmail.com>
2020-03-08 22:42:49 -0300

    master: Alterando retorno da api para ResponseEntity.

--- src/main/java/br/com/carc2ccadastro/resources/CarroRestController.java
+++ src/main/java/br/com/carc2ccadastro/resources/CarroRestController.java
@@ -4,6 +4,7 @@ package br.com.carc2ccadastro.resources;
 import br.com.carc2ccadastro.domain.Carro;
 import br.com.carc2ccadastro.service.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,8 @@ public class CarroRestController {
     private CarroService carroService;
 
     @GetMapping("/carro/{id}")
-    public Carro getCarroByUuid(@PathVariable("id") Long id){
-
-         return carroService.getCarroById(id);
+    public ResponseEntity<Carro> getCarroByUuid(@PathVariable("id") Long id){
+         return ResponseEntity.ok(carroService.getCarroById(id));
     }
 
 
