2f012817880b837394c4a3d25214baf5a6912b66
gustavoaquino <guuaquino@hotmail.com>
2020-03-15 15:55:07 -0300

    master: Espaçamento apago.

--- src/main/java/br/com/carc2ccadastro/controller/IndexController.java
+++ src/main/java/br/com/carc2ccadastro/controller/IndexController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
     @GetMapping("/index")
     public String getIndex(){
         return "index";
     }
-
 }
