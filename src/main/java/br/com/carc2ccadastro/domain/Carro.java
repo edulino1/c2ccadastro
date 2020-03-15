ff6812f8a6b9e2d4e403dabdbc22dcc9b060da82
gustavoaquino <guuaquino@hotmail.com>
2020-03-15 16:20:34 -0300

    master: Criando métodos de acesso ao banco.

--- src/main/java/br/com/carc2ccadastro/domain/Carro.java
+++ src/main/java/br/com/carc2ccadastro/domain/Carro.java
@@ -1,9 +1,81 @@
 package br.com.carc2ccadastro.domain;
 
+import br.com.carc2ccadastro.hardcode.TipoCarro;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Carro {
 
-    /*Inserir métodos Getter e Setter*/
+    @Id
+    private Long id;
+
+    private String nome;
+
+    private String modelo;
+
+    private TipoCarro tipo;
+
+    private String placa;
+
+    private String cor;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public TipoCarro getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCarro tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
+
