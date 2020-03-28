300f14d0b9d55e8601a0ef25332af63e2558bd62
gustavoaquino <guuaquino@hotmail.com>
2020-03-28 19:42:08 -0300

    master: Atualizando API

--- src/main/java/br/com/carc2ccadastro/resources/CarroRestController.java
+++ src/main/java/br/com/carc2ccadastro/resources/CarroRestController.java
@@ -4,22 +4,36 @@ package br.com.carc2ccadastro.resources;
 import br.com.carc2ccadastro.domain.Carro;
 import br.com.carc2ccadastro.service.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
-@RestController("app/api")
+
+@RestController
+@RequestMapping("/app/api")
 public class CarroRestController {
 
     @Autowired
     private CarroService carroService;
 
-    @GetMapping("/carro/{id}")
-    public ResponseEntity<Carro> getCarroByUuid(@PathVariable("id") Long id){
-        // return ResponseEntity.ok(carroService.getAllCars(id));
-        return null;
+    @GetMapping("/carros")
+    public ResponseEntity<List<Carro>> getAllCarros(){
+
+        List<Carro> carro = carroService.getAllCars();
+
+        return new ResponseEntity<>(carro, HttpStatus.OK);
     }
 
+    @PostMapping("/carro/salvar")
+    public ResponseEntity<Carro> postCarro(Carro carro){
 
+        carroService.postCarro(carro);
+
+        return new ResponseEntity<>(carro, HttpStatus.CREATED);
+    }
 }
+
