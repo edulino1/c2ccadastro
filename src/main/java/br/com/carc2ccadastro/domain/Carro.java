7e6e3a47245af75fa7806ce965a4a8401f53daaa
gustavoaquino <guuaquino@hotmail.com>
2020-03-28 19:41:46 -0300

    master: Atualizando domain

--- src/main/java/br/com/carc2ccadastro/domain/Carro.java
+++ src/main/java/br/com/carc2ccadastro/domain/Carro.java
@@ -1,13 +1,17 @@
 package br.com.carc2ccadastro.domain;
 
 import br.com.carc2ccadastro.hardcode.TipoCarro;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Carro {
 
     @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
