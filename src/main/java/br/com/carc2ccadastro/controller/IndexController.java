1bcfbed6826d2c1bf6c05b8af1c831fb53a16dc6
gustavoaquino <guuaquino@hotmail.com>
2020-04-05 19:12:29 -0300

    master: Adicionando novo método em IndexController.

--- src/main/java/br/com/carc2ccadastro/controller/IndexController.java
+++ src/main/java/br/com/carc2ccadastro/controller/IndexController.java
@@ -1,13 +1,15 @@
 package br.com.carc2ccadastro.controller;
 
-
 import br.com.carc2ccadastro.domain.Carro;
 import br.com.carc2ccadastro.service.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,22 +17,31 @@ public class IndexController {
     @Autowired
     private CarroService carroService;
 
-    @GetMapping("/index")
-    public String getIndex(){
+    @GetMapping("/")
+    public String getIndex(Model model, Carro carro){
         return "index";
     }
 
+    @GetMapping("/listar")
+    public String getLista(Model model, Carro carro){
 
-    @PostMapping("/salvar")
-    public String postIndex(BindingResult bindingResult, Carro carro){
+        List<Carro> listaCarros = carroService.getAllCars();
+
+        model.addAttribute("listaCarros", listaCarros);
+
+        return "lista-carros";
+    }
+
+    @PostMapping("/")
+    public String postIndex(Model model, Carro carro, BindingResult bindingResult){
 
         if(bindingResult.hasErrors()){
-            return "Tem coisa errada ai irmão";
+            return "Erro ao salvar!";
         }
 
         carroService.postCarro(carro);
 
-        return getIndex();
+        return "redirect:/";
    }
 
 }
