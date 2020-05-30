1f30b807f56dbf8ae239c52a2bdcea2daec420ce
gustavoaquino <guuaquino@hotmail.com>
2020-05-30 19:03:30 -0300

    master: Atualizando LoginSecurity.

---
+++ src/main/java/br/com/carc2ccadastro/security/LoginSecurity.java
@@ -0,0 +1,11 @@
+package br.com.carc2ccadastro.security;
+
+import br.com.carc2ccadastro.domain.Usuario;
+
+public class LoginSecurity {
+
+    public static Boolean logado = Boolean.FALSE;
+    
+    public static Usuario usuario;
+
+}
