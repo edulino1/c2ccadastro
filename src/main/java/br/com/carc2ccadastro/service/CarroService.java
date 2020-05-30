90d6b0b0f82c2de80889685ad48a6a55d748e7cc
gustavoaquino <guuaquino@hotmail.com>
2020-05-30 19:03:58 -0300

    master: criando CarroService.

--- src/main/java/br/com/carc2ccadastro/service/CarroService.java
+++ src/main/java/br/com/carc2ccadastro/service/CarroService.java
@@ -1,5 +1,6 @@
 package br.com.carc2ccadastro.service;
 
+import br.com.carc2ccadastro.dao.CarroDAO;
 import br.com.carc2ccadastro.domain.Carro;
 import br.com.carc2ccadastro.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,16 @@ public class CarroService {
 
     @Autowired
     private CarroRepository carroRepository;
+    
+    @Autowired
+    private CarroDAO carroDAO;
 
-    public List<Carro> getAllCars(){
-       return carroRepository.findAll();
+    public List<Carro> findListCarByLogin(Long usuarioId){
+       return carroDAO.findCarroByUsuarioId(usuarioId);
+    }
+    
+    public List<Carro> findAllCars(){
+        return carroRepository.findAll();
     }
 
     public Optional<Carro> getCarroById(Long id){
