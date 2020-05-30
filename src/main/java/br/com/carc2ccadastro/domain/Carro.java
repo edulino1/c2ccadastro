1c6b5b76615e512e2dfbf0ab3ee04cb4c69dd458
gustavoaquino <guuaquino@hotmail.com>
2020-05-30 19:01:43 -0300

    master: Atualizando domain de carro.

--- src/main/java/br/com/carc2ccadastro/domain/Carro.java
+++ src/main/java/br/com/carc2ccadastro/domain/Carro.java
@@ -23,61 +23,81 @@ public class Carro {
     private String cor;
 
     private String descricao;
-
+    
+    private Long usuarioId;
+    
+    private Boolean statusCarro;
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getMarca() {
         return marca;
     }
-
+    
     public void setMarca(String marca) {
         this.marca = marca;
     }
-
+    
     public String getModelo() {
         return modelo;
     }
-
+    
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-
+    
     public TipoCarro getTipo() {
         return tipo;
     }
-
+    
     public void setTipo(TipoCarro tipo) {
         this.tipo = tipo;
     }
-
+    
     public String getPlaca() {
         return placa;
     }
-
+    
     public void setPlaca(String placa) {
         this.placa = placa;
     }
-
+    
     public String getCor() {
         return cor;
     }
-
+    
     public void setCor(String cor) {
         this.cor = cor;
     }
-
+    
     public String getDescricao() {
         return descricao;
     }
-
+    
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+    
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+    
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    
+    public Boolean getStatusCarro() {
+        return statusCarro;
+    }
+    
+    public void setStatusCarro(Boolean statusCarro) {
+        this.statusCarro = statusCarro;
     }
 }
 
