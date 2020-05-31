fd7c67c6c92151141d5d05f0e863377181d179a1
gustavoaquino <guuaquino@hotmail.com>
2020-05-31 16:04:48 -0300

    master: Alterando CarroDAO.

--- src/main/java/br/com/carc2ccadastro/dao/CarroDAO.java
+++ src/main/java/br/com/carc2ccadastro/dao/CarroDAO.java
@@ -5,5 +5,8 @@ import br.com.carc2ccadastro.domain.Carro;
 import java.util.List;
 
 public interface CarroDAO {
+	
 	List<Carro> findCarroByUsuarioId(Long usuarioId);
+	
+	List<Carro> findCarroByUsuarioAluguelId(Long usuarioAluguelId);
 }
