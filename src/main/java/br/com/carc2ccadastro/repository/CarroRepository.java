442bf4e14db5a0b28ef15970f3d5b269e5760631
gustavoaquino <guuaquino@hotmail.com>
2020-06-01 17:24:50 -0300

    master: Atualizando API

--- src/main/java/br/com/carc2ccadastro/repository/CarroRepository.java
+++ src/main/java/br/com/carc2ccadastro/repository/CarroRepository.java
@@ -2,8 +2,14 @@ package br.com.carc2ccadastro.repository;
 
 import br.com.carc2ccadastro.domain.Carro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CarroRepository extends JpaRepository<Carro, Long> {
+	
+	@Query(value = "SELECT * FROM CARRO WHERE STATUS_CARRO IS FALSE", nativeQuery = true)
+	List<Carro> findAll();
 }
